8c3ab690dafb6e6be66694c7e6a2643a5d4dccb4
Todd Walraven <cod4247@yahoo.com>
2020-02-28 08:22:19 -0800

    add base files to project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Pastry.Models;
+
+namespace PastryShop
+{
+  public class Program
+  {
+    public static void Main()
+    {
+
+    }
+  }
+}
