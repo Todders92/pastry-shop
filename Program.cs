9cef0439cd003110f8d60c27825977fca08666aa
Todd Walraven <cod4247@yahoo.com>
2020-03-08 21:13:00 -0700

    add branching allowing user to buy only bread or only pastry

--- Program.cs
+++ Program.cs
@@ -25,18 +25,35 @@ namespace PastryShop
       int breadNumber = int.Parse(breadResponse);
       BreadOrder bread = new BreadOrder(breadNumber);
       int cost = bread.AddBreadPrice(breadNumber);
-      Console.WriteLine("this is the total price for the bread order: $" + cost);
-     
-      ///////////////////////pastry logic ///////////////////////////
+      Console.WriteLine("would you like to buy some pastries?[y] yes [n] no");
+      string answer = Console.ReadLine();
+      if (answer.ToLower() == "y")
+      {
+        Console.WriteLine("how many orders of pastry would you like?");
+        string pastryResponse = Console.ReadLine();
+        int pastryNumber = int.Parse(pastryResponse);
+        PastryOrder pastry = new PastryOrder(pastryNumber);
+        int numberResponse2 = pastry.AddPastry(pastryNumber);
+        pastry.AddPastryPrice(numberResponse2);
+        Console.WriteLine("this is the total price for the pastry order: $" + pastry.Price);
+        Console.WriteLine("your order total is $" + (pastry.Price + bread.BreadPrice) + ".00");
+      }
+      else if (answer.ToLower() == "n")
+      {
+        Console.WriteLine("your order total is $" + (bread.BreadPrice) + ".00");
+        Console.WriteLine("Thank you for coming in today, enjoy your bread!");
+      }
+      else if (answer.ToLower() == "y")
+      {
       Console.WriteLine("how many orders of pastry would you like?");
       string pastryResponse = Console.ReadLine();
       int pastryNumber = int.Parse(pastryResponse);
       PastryOrder pastry = new PastryOrder(pastryNumber);
       int numberResponse2 = pastry.AddPastry(pastryNumber);
       pastry.AddPastryPrice(numberResponse2);
-      Console.WriteLine("this is the total price for the pastry order: $" + pastry.Price);
-      Console.WriteLine("your order total is $" + (pastry.Price + cost) + ".00");
-
+      Console.WriteLine("your order total is $" + (pastry.Price + bread.BreadPrice) + ".00");
+      Console.WriteLine("thank you for coming in today! Enjoy!");
+      }
     }
   }
 }
